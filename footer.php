dff78c03e3054a723ac80e93c6a07f2f2b366331
stefwilliams <stefwilliams@gmail.com>
2015-06-09 21:03:56 +0100

    Social Media icons and link to RIBA

--- footer.php
+++ footer.php
@@ -5,7 +5,14 @@
           <p>&copy; <?php echo date('Y'); ?> kotzmuth-williams architects</p>
         </div>
         <div class="span9">
-          <p class="pull-right">Sophia House, 28 Cathedral Road, Cardiff, CF11 9JW. T: 0845 459 9944</p>
+          <p class="pull-right">Sophia House, 28 Cathedral Road, Cardiff, CF11 9LJ. T: 0845 459 9944</p>
+        </div>
+        <div class="span12 text-center">
+          <a href="https://www.facebook.com/pages/Kotzmuth-Williams-Architects/440582156102777"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/facebook.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/facebook-gs.png'" alt="Facebook"/></a>
+          <a href="https://twitter.com/KW_Architects"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/twitter.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/twitter-gs.png'" alt="Twitter"/></a>
+          <a href="https://www.pinterest.com/sion0679/"><img src="<?php echo get_template_directory_uri(); ?>/img/pinterest-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/pinterest.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/pinterest-gs.png'" alt="Pinterest"/></a>
+          <a href="https://instagram.com/kotzmuth_williams_architects/"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/instagram.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/instagram-gs.png'" alt="Instagram"/></a>
+          <a href="http://www.houzz.com/uk/pro/majakotzmuthclarke/kotzmuth-williams-architects"><img src="<?php echo get_template_directory_uri(); ?>/img/houzz-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/houzz.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/houzz-gs.png'" alt="Houzz"/></a>
         </div>
       </div>
     </div>
--- front-page.php
+++ front-page.php
@@ -20,7 +20,7 @@
 <!-- div class="container content" -->
 	<div class="row">
 		<div class="span3">
-			<img src="<?php echo get_template_directory_uri(); ?>/img/riba-chartered-logo.png" alt="RIBA Chartered Practice"/>
+			<a href="http://www.architecture.com/FindAnArchitect/ArchitectPractices/kotzmuth-williamsarchitects/kotzmuth-williamsarchitects.aspx"><img src="<?php echo get_template_directory_uri(); ?>/img/riba-chartered-logo.png" alt="RIBA Chartered Practice"/></a>
 			<p><?php _e('Directors', 'kwa'); ?>:</p>
 			<p>Maja Kotzmuth-Clarke <br />BSc, B(Arch), RIBA</p>
 			<p>Siôn Williams <br />BSc, B(Arch), RIBA</p>
