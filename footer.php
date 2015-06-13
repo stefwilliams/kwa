e00e734a4075831fc4bf35bfceec61b306f7353c
stefwilliams <stefwilliams@gmail.com>
2015-06-13 15:01:15 +0100

    Social icons tweaks

--- footer.php
+++ footer.php
@@ -1,22 +1,21 @@
 <!-- div class="container" -->
-    <div class="footer">
-      <div class="row">
-        <div class="span3">
-          <p>&copy; <?php echo date('Y'); ?> kotzmuth-williams architects</p>
-        </div>
-        <div class="span9">
-          <p class="pull-right">Sophia House, 28 Cathedral Road, Cardiff, CF11 9LJ. T: 0845 459 9944</p>
-        </div>
-        <div class="span12 text-center">
-          <a href="https://www.facebook.com/pages/Kotzmuth-Williams-Architects/440582156102777"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/facebook.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/facebook-gs.png'" alt="Facebook"/></a>
-          <a href="https://twitter.com/KW_Architects"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/twitter.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/twitter-gs.png'" alt="Twitter"/></a>
-          <a href="https://www.pinterest.com/sion0679/"><img src="<?php echo get_template_directory_uri(); ?>/img/pinterest-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/pinterest.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/pinterest-gs.png'" alt="Pinterest"/></a>
-          <a href="https://instagram.com/kotzmuth_williams_architects/"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/instagram.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/instagram-gs.png'" alt="Instagram"/></a>
-          <a href="http://www.houzz.com/uk/pro/majakotzmuthclarke/kotzmuth-williams-architects"><img src="<?php echo get_template_directory_uri(); ?>/img/houzz-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/houzz.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/houzz-gs.png'" alt="Houzz"/></a>
-        </div>
-      </div>
+<div class="footer">
+  <div class="row">
+    <div class="span6">
+      <a href="https://www.facebook.com/pages/Kotzmuth-Williams-Architects/440582156102777"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/facebook.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/facebook-gs.png'" alt="Facebook"/></a>
+      <a href="https://twitter.com/KW_Architects"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/twitter.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/twitter-gs.png'" alt="Twitter"/></a>
+      <a href="https://www.pinterest.com/sion0679/"><img src="<?php echo get_template_directory_uri(); ?>/img/pinterest-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/pinterest.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/pinterest-gs.png'" alt="Pinterest"/></a>
+      <a href="https://instagram.com/kotzmuth_williams_architects/"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/instagram.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/instagram-gs.png'" alt="Instagram"/></a>
+      <a href="http://www.houzz.com/uk/pro/majakotzmuthclarke/kotzmuth-williams-architects"><img src="<?php echo get_template_directory_uri(); ?>/img/houzz-gs.png" onmouseover="this.src='<?php echo get_template_directory_uri(); ?>/img/houzz.png'" onmouseout="this.src='<?php echo get_template_directory_uri(); ?>/img/houzz-gs.png'" alt="Houzz"/></a>
+    
     </div>
+    <div class="span6">
+      <p class="pull-right">&copy; <?php echo date('Y'); ?> kotzmuth-williams architects</p>
+    </div>
+
   </div>
+</div>
+</div>
 
 <?php wp_footer(); ?>
 </body>
